716afe8f6815ca79571af292d45d7623b179f0c6
LeoReubelt <leo.reubelt@outlook.com>
2016-08-12 14:17:45 -0400

    First commit.

---
+++ ObjectListable/ObjectListable.h
@@ -0,0 +1,19 @@
+//
+//  ObjectListable.h
+//  ObjectListable
+//
+//  Created by Leo Reubelt on 8/12/16.
+//  Copyright © 2016 Enhatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectListable.
+FOUNDATION_EXPORT double ObjectListableVersionNumber;
+
+//! Project version string for ObjectListable.
+FOUNDATION_EXPORT const unsigned char ObjectListableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectListable/PublicHeader.h>
+
+
